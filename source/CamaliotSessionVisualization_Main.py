69d6993994238e68662841d5ec04f2d89d40afce
Vasileios Vlachakis <vvlachak@gmail.com>
2022-10-05 00:05:08 +0200

    Update comments

--- source/CamaliotSessionVisualization_Main.py
+++ source/CamaliotSessionVisualization_Main.py
@@ -5,9 +5,9 @@
     The folder must be located in the "\data" folder of the application.
     
     The user should sequentially use the following functions in order to prepare the data for visualization.
-        dataDictionary:      It obtains the required data
-        dict2json:           Stores the required data in JSON format
-        
+        dataDictionary:      To obtain the required data
+        dict2json:           To store the required data in JSON format
+
     Optionally, the function dict2csv can be used to get a list of the latitudes and longitudes of the measurement points.
       
     For the visualization of the data, one or more of the following functions should be called.
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     # Get the list of dictionaries "dataDictionary" and store the values in the {__PROJECTNAME__}.JSON file in the "\data" folder
     dict2json(dataDictionary, __PROJECTNAME__)
 
-    # Get the list of dictionaries "dataDictionary" and store the values in the {__PROJECTNAME__}.CSV file in the "\data" folder
+    # Get the list of dictionaries "dataDictionary" and store the longitude and latitude values in the {__PROJECTNAME__}.CSV file in the "\data" folder
     dict2csv(dataDictionary, __PROJECTNAME__)
 
     # Get the path of the JSON file
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     # Plot the cumulative number of the measurements for each GNSS system (stored in the "\figures" folder)
     plotMeasCountPerWeek(__PROJECTNAME__, file = jsonInFilename, format=['pdf', 'png', 'jpg'])
 
-    # To plot the histogram of the duration of the sessions (stored in the "\figures" folder)
+    # Plot the histogram of the duration of the sessions (stored in the "\figures" folder)
     plotDurationHistogram(__PROJECTNAME__, file = jsonInFilename) # format=['pdf', 'png', 'jpg']
 
  
